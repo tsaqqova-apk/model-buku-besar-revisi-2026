5a1e69fd8370e27c35b59c739ba2c373a030b3a0
tsaqqova-apk <tsaqqova.apk@gmail.com>
2026-09-21 08:33:24 +0700

    Add files via upload

---
+++ 073/settings/bookmark_config.js
@@ -0,0 +1,13 @@
+﻿var ols=[
+{caption:"ترجمة المؤلف",page:"1",url:"",level:"1",children:[]},
+{caption:"خطبة الكتاب",page:"3",url:"",level:"1",children:[]},
+{caption:"المقدمات",page:"5",url:"",level:"1",children:[]},
+{caption:"الكتاب الأول في الكتاب",page:"11",url:"",level:"1",children:[]},
+{caption:"الكتاب الثاني في السنة",page:"31",url:"",level:"1",children:[]},
+{caption:"الكتاب الثالث في الإجماع",page:"39",url:"",level:"1",children:[]},
+{caption:"الكتاب الرابع في القياس",page:"41",url:"",level:"1",children:[]},
+{caption:"الكتاب الخامس في الاستدلال",page:"57",url:"",level:"1",children:[]},
+{caption:"الكتاب السادس في التعادل والتراجيح",page:"60",url:"",level:"1",children:[]},
+{caption:"الكتاب السابع في الاجتهاد",page:"66",url:"",level:"1",children:[]},
+{caption:"الخاتمة في مبادئ التصوف",page:"76",url:"",level:"1",children:[]}
+];
