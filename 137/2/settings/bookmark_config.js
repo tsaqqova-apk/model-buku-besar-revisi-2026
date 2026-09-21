e82ca75a61dc824cceeb9dc87644a2a5a49da13e
tsaqqova-apk <tsaqqova.apk@gmail.com>
2026-09-21 09:13:58 +0700

    Add files via upload

---
+++ 137/2/settings/bookmark_config.js
@@ -0,0 +1,12 @@
+﻿var ols=[
+{caption:"1 صفحة",page:"1",url:"",level:"1",children:[]},
+{caption:"2 صفحة",page:"2",url:"",level:"1",children:[]},
+{caption:"3 صفحة",page:"3",url:"",level:"1",children:[]},
+{caption:"4 صفحة",page:"4",url:"",level:"1",children:[]},
+{caption:"5 صفحة",page:"5",url:"",level:"1",children:[]},
+{caption:"6 صفحة",page:"6",url:"",level:"1",children:[]},
+{caption:"7 صفحة",page:"7",url:"",level:"1",children:[]},
+{caption:"8 صفحة",page:"8",url:"",level:"1",children:[]},
+{caption:"9 صفحة",page:"9",url:"",level:"1",children:[]},
+{caption:"10 صفحة",page:"10",url:"",level:"1",children:[]}
+];
