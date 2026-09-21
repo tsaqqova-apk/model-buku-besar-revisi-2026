e445cb713f79b235477a19f77b622751b62e1bc4
tsaqqova-apk <tsaqqova.apk@gmail.com>
2026-09-21 07:34:29 +0700

    Add files via upload

---
+++ 067/settings/bookmark_config.js
@@ -0,0 +1,18 @@
+﻿var ols=[
+{caption:"كتاب أحكام الطهارة",page:"3",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الصلاة",page:"11",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الزكاة",page:"22",url:"",level:"1",children:[]},
+{caption:"كتاب بيان أحكام الصيام",page:"25",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الحج",page:"27",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام البيوع وغيرها من المعاملات",page:"30",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الفرائض والوصايا",page:"41",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام النكاح وما يتعلق به",page:"43",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الجنايات",page:"53",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الحدود",page:"56",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الجهاد",page:"58",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الصيد والذبائح والضحايا والأطعمة",page:"61",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام السبق والرمى",page:"64",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الأيمان والنذور",page:"64",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام الأقضية والشهادات",page:"65",url:"",level:"1",children:[]},
+{caption:"كتاب أحكام العتق",page:"69",url:"",level:"1",children:[]}
+];
